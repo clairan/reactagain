3c860f6890ad6b069104a4bbc33e40aac1391baa
Andreas Clair <andreas.clair@gmail.com>
2023-04-18 16:16:02 +0200

    trying to scrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+const driver = await new Builder()
+  .forBrowser('chrome')
+  .setChromeOptions(new chrome.Options().headless())
+  .build();
+
+  function scrap() {
+  driver.get('https://eservicetest.helsingborg.se/h130');
+  let nextButton =  driver.findElement(By.className('nextbuttonlink'));
+  nextButton.click();
+  const inputs = driver.executeScript('return document.getElementsByTagName("input");');
+  return inputs.length;
+ } 
+
 
 function App() {
+  scrap();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The page has scrap() number of input elements!
         </p>
         <a
           className="App-link"
